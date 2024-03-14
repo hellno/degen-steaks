0f9690ad5feaa0256ee37a733f4e5942445ce610
valleXYZ <valentin.seehausen@gmail.com>
2024-03-14 21:58:24 +0100

    BetRegistryContract_BetCashedOut_handler

--- indexer/src/event-handlers/BetRegistry.ts
+++ indexer/src/event-handlers/BetRegistry.ts
@@ -1,4 +1,6 @@
 import {
+  BetRegistryContract_BetCashedOut_handler,
+  BetRegistryContract_BetCashedOut_loader,
   BetRegistryContract_BetPlaced_handler,
   BetRegistryContract_BetPlaced_loader,
   BetRegistryContract_MarketCreated_handler,
@@ -18,6 +20,8 @@ BetRegistryContract_MarketCreated_handler(({ event, context }) => {
     user = {
       id: event.params.creator,
       creatorFeeReceived: 0n,
+      totalDegenWon: 0n,
+      totalDegenBet: 0n,
     };
     context.User.set(user);
   }
@@ -37,6 +41,7 @@ BetRegistryContract_MarketCreated_handler(({ event, context }) => {
     totalDegen: undefined,
     endPrice: undefined,
     creatorFee: undefined,
+    highWon: undefined,
   };
   context.Market.set(market);
 });
@@ -56,9 +61,15 @@ BetRegistryContract_BetPlaced_handler(({ event, context }) => {
     user = {
       id: event.params.user,
       creatorFeeReceived: 0n,
+      totalDegenWon: 0n,
+      totalDegenBet: 0n,
     };
-    context.User.set(user);
   }
+  user = {
+    ...user,
+    totalDegenBet: user.totalDegenBet + event.params.degen,
+  };
+  context.User.set(user);
 
   let market = context.Market.get(event.params.marketId.toString());
   if (market === undefined) {
@@ -79,6 +90,9 @@ BetRegistryContract_BetPlaced_handler(({ event, context }) => {
       user_id: user.id,
       sharesHigher: 0n,
       sharesLower: 0n,
+      cashedOut: false,
+      cashedOutDegen: undefined,
+      cashOutTransaction: undefined,
     };
   }
 
@@ -117,7 +131,7 @@ BetRegistryContract_BetPlaced_handler(({ event, context }) => {
   context.Market.set(market);
   context.Bet.set(bet);
 
-  const PlacedBet = {
+  const placedBet = {
     id:
       event.params.marketId.toString() +
       "-" +
@@ -132,9 +146,10 @@ BetRegistryContract_BetPlaced_handler(({ event, context }) => {
     degen: event.params.degen,
     steaks: event.params.steaks,
     feeSteaks: event.params.feeSteaks,
+    transaction: event.transactionHash,
   };
 
-  context.PlacedBet.set(PlacedBet);
+  context.PlacedBet.set(placedBet);
 });
 
 BetRegistryContract_MarketResolved_loader(({ event, context }) => {
@@ -173,6 +188,78 @@ BetRegistryContract_MarketResolved_handler(({ event, context }) => {
     totalDegen: event.params.totalDegen,
     creatorFee: event.params.creatorFee,
     isResolved: true,
+    highWon: event.params.endPrice > market.targetPrice,
   };
   context.Market.set(market);
+});
+
+BetRegistryContract_BetCashedOut_loader(({ event, context }) => {
+  context.Bet.load(
+    event.params.marketId.toString() + "-" + event.params.user,
+    {}
+  );
+  context.User.load(event.params.user);
+  context.Market.load(event.params.marketId.toString(), {});
+});
+
+BetRegistryContract_BetCashedOut_handler(({ event, context }) => {
+  let market = context.Market.get(event.params.marketId.toString());
+  if (market === undefined) {
+    context.log.error(
+      "BetRegistry::BetCashedOut: Market not found: " +
+        event.params.marketId.toString()
+    );
+    return;
+  }
+
+  let user = context.User.get(event.params.user);
+  if (user === undefined) {
+    context.log.error(
+      "BetRegistry::BetCashedOut: User not found: " + event.params.user
+    );
+    return;
+  }
+
+  let bet = context.Bet.get(
+    event.params.marketId.toString() + "-" + event.params.user
+  );
+  if (bet === undefined) {
+    context.log.error(
+      "BetRegistry::BetCashedOut: Bet not found: " +
+        event.params.marketId.toString() +
+        "-" +
+        event.params.user
+    );
+    return;
+  }
+
+  if (market.highWon) {
+    market = {
+      ...market,
+      totalSharesHigher: market.totalSharesHigher - event.params.marketShares,
+    };
+  } else {
+    market = {
+      ...market,
+      totalSharesLower: market.totalSharesLower - event.params.marketShares,
+    };
+  }
+
+  market = {
+    ...market,
+    totalDegen: market.totalDegen! - event.params.degen,
+  };
+  context.Market.set(market);
+
+  bet = {
+    ...bet,
+    cashedOut: true,
+    cashedOutDegen: event.params.degen,
+    cashOutTransaction: event.transactionHash,
+  };
+
+  user = {
+    ...user,
+    totalDegenWon: user.totalDegenWon + event.params.degen,
+  };
 });
