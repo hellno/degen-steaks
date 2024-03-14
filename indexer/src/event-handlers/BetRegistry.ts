8d4905b8e99ee881f379494c01f541216bb64ce2
valleXYZ <valentin.seehausen@gmail.com>
2024-03-14 20:38:23 +0100

    add BetRegistryContract_MarketCreated_handler

--- indexer/src/event-handlers/BetRegistry.ts
+++ indexer/src/event-handlers/BetRegistry.ts
@@ -1,4 +1,4 @@
-import { BetRegistryContract_MarketCreated_loader } from "../src/Handlers.gen";
+import { BetRegistryContract_MarketCreated_handler, BetRegistryContract_MarketCreated_loader } from "../src/Handlers.gen";
 
 
 BetRegistryContract_MarketCreated_loader(({ event, context }) => {
@@ -6,3 +6,28 @@ BetRegistryContract_MarketCreated_loader(({ event, context }) => {
   context.User.load(event.params.creator);
 });
 
+
+BetRegistryContract_MarketCreated_handler(({ event, context }) => {
+  let user = context.User.get(event.params.creator);
+  if (user === undefined) {
+    user = {
+      id: event.params.creator,
+    }
+    context.User.set(user);
+  }
+
+  const market = {
+    id: event.params.id.toString(),
+    creator_id: user.id,
+    startTime: event.blockTimestamp,
+    targetPrice: event.params.targetPrice,
+    endTime: event.params.endTime,
+    totalHigher: 0n,
+    totalLower: 0n,
+    totalSteakedDegen: 0n,
+    totalDegen: undefined,
+    endPrice: undefined,
+
+  }
+  context.Market.set(market);
+})
