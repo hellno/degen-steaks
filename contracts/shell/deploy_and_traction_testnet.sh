70c2793d50720e227a385b4148b5cb7d7d9bee80
valleXYZ <valentin.seehausen@gmail.com>
2024-03-15 18:38:53 +0100

    deploy testnet

--- contracts/shell/deploy_and_traction_testnet.sh
+++ contracts/shell/deploy_and_traction_testnet.sh
@@ -5,16 +5,16 @@ source .env
 
 # To deploy and verify our contract
 NETWORK=testnet forge script script/testnet/Deployment.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
-NETWORK=testnet STEP=1 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=1 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
 echo "start sleep"
 sleep 90
 echo "end sleep"
-NETWORK=testnet STEP=2 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
-NETWORK=testnet STEP=3 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
-NETWORK=testnet STEP=4 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
-NETWORK=testnet STEP=5 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=2 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
+NETWORK=testnet STEP=3 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
+NETWORK=testnet STEP=4 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
+NETWORK=testnet STEP=5 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
 echo "start sleep"
 sleep 90
 echo "end sleep"
-NETWORK=testnet STEP=6 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
-NETWORK=testnet STEP=7 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=6 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
+NETWORK=testnet STEP=7 MARKET_DURATION=90 forge script script/testnet/Traction.s.sol --rpc-url https://sepolia.base.org --broadcast -vvvv 
