5d41d5735e2cbd2abe51538e0f224c4a1c1bedfa
valleXYZ <valentin.seehausen@gmail.com>
2024-03-15 17:00:46 +0100

    change network to testnet

--- contracts/shell/deploy_and_traction_testnet.sh
+++ contracts/shell/deploy_and_traction_testnet.sh
@@ -10,17 +10,17 @@ cast rpc anvil_setBalance 0x42A1EeeAFf98C2C713c5Bf253057564E5a171306 10000000000
 cast rpc anvil_setBalance 0x3E8C992a2e94280be9f2b942Bfcc3e2a7cc30e71 1000000000000000000
 
 # To deploy and verify our contract
-NETWORK=local forge script script/testnet/Deployment.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
-NETWORK=local STEP=1 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet forge script script/testnet/Deployment.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=1 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
 echo "start sleep"
 sleep 60
 echo "end sleep"
-NETWORK=local STEP=2 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
-NETWORK=local STEP=3 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
-NETWORK=local STEP=4 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
-NETWORK=local STEP=5 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=2 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=3 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=4 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=5 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
 echo "start sleep"
 sleep 60
 echo "end sleep"
-NETWORK=local STEP=6 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
-NETWORK=local STEP=7 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=6 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
+NETWORK=testnet STEP=7 MARKET_DURATIONH=90 forge script script/testnet/Traction.s.sol --rpc-url http://localhost:8545 --broadcast -vvvv --legacy 
