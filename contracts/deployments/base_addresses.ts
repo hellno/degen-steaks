a17631bf7913421672586f5a3a3e436e65240e4e
Nate Faber <nathaniel@njfaber.com>
2024-04-03 11:57:09 +0200

    Deploy to base (#28)

--- contracts/deployments/base_addresses.ts
+++ contracts/deployments/base_addresses.ts
@@ -1,4 +1,4 @@
-export const priceFeedAddress = "0x786f4e0130e21281c2fc76B29A84Bbb65B2028EB";
+export const priceFeedAddress = "0x28836Bf1909Cd044bA36c20d44fb39e24EF838c3";
 export const degenTokenAddress = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed";
-export const steakedDegenAddress = "0xa30176701DB4Df8023516FFaa9Ee8cBC98E0d359";
-export const betRegistryAddress = "0x8aA47373bb61D5CA93098182420A454B7039d1a2";
+export const steakedDegenAddress = "0xa523a694A72Eac635F78e7562D754E218E72F933";
+export const betRegistryAddress = "0x7C4e621ce078fe39B205f90AEd2C1371794B12A9";
