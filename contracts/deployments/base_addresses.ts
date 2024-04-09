3c4c39003d65f2371f13836670f2599006d84d7b
valleXYZ <valentin.seehausen@gmail.com>
2024-04-09 17:01:07 +0200

    deploy to base mainnet

--- contracts/deployments/base_addresses.ts
+++ contracts/deployments/base_addresses.ts
@@ -1,4 +1,4 @@
-export const priceFeedAddress = "0x28836Bf1909Cd044bA36c20d44fb39e24EF838c3";
+export const priceFeedAddress = "0xA6554cE636a0031DC9928f6219BeeeD4aCEAF0c7";
 export const degenTokenAddress = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed";
-export const steakedDegenAddress = "0xa523a694A72Eac635F78e7562D754E218E72F933";
-export const betRegistryAddress = "0x7C4e621ce078fe39B205f90AEd2C1371794B12A9";
+export const steakedDegenAddress = "0xC20e615356D3301bF3eC624585e8c9C88a2096E4";
+export const betRegistryAddress = "0x55a332d75f2BC9937d50084b8D4af7cB9Fa192Fe";
